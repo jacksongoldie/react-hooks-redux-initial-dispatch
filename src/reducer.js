1a237a036f6254d42b0c99de296c766a43039a09
jacksongoldie <goldiejackson25@yahoo.com>
2022-05-10 09:33:40 -0500

    complete

--- src/reducer.js
+++ src/reducer.js
@@ -1,20 +1,45 @@
-let state = { count: 0 };
+let state;
 
-function changeState(state, action) {
-  switch (action.type) {
-    case "counter/increment":
+// function changeState(state = { count: 0 }, action) {
+//   switch (action.type) {
+//     case "counter/increment":
+//       return { count: state.count + 1 };
+//     default:
+//       return state;
+//   }
+// }
+
+// function dispatch(action) {
+//   state = changeState(state, action);
+//   render();
+// }
+
+// function render() {
+//   const app = document.querySelector("#app");
+//   app.textContent = state.count;
+// }
+
+// dispatch({ type: '@@INIT' })
+
+function changeState(state = { count: 3 }, action){
+  switch(action.type) {
+    case "count/increment":
       return { count: state.count + 1 };
+    case "count/decrement":
+      return { count: state.count - 1 };
     default:
       return state;
   }
 }
 
-function dispatch(action) {
-  state = changeState(state, action);
-  render();
+function dispatch(action){
+  state = changeState(state, action)
+  render()
 }
 
 function render() {
-  const app = document.querySelector("#app");
-  app.textContent = state.count;
+  let app = document.getElementById("app")
+  app.textContent = state.count
 }
+
+dispatch({ type: "@@INIT" })
