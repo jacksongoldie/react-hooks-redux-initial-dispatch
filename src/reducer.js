1c6b5fbff95a6540ef46ff3cd05c16888a9f9e73
jacksongoldie <goldiejackson25@yahoo.com>
2022-05-10 09:29:38 -0500

    commit tree 4c6fc3afa737d7dd591d78b30b9e053109e838fe

--- src/reducer.js
+++ src/reducer.js
@@ -1,20 +1,45 @@
-let state = { count: 0 };
+let state;
 
-function changeState(state, action) {
-  switch (action.type) {
+// function changeState(state = { count: 0 }, action) {
+//   switch (action.type) {
+//     case "counter/increment":
+//       return { count: state.count + 1 };
+//     default:
+//       return state;
+//   }
+// }
+
+// function dispatch(action) {
+//   state = changeState(state, action);
+//   render();
+// }
+
+// function render() {
+//   const app = document.querySelector("#app");
+//   app.textContent = state.count;
+// }
+
+// dispatch({ type: '@@INIT' })
+
+function changeState(state, action){
+  switch(action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
+    case "counter/decrement":
+      return { count: state.count - 1 };
     default:
       return state;
   }
 }
 
-function dispatch(action) {
-  state = changeState(state, action);
-  render();
+function dispatch(action){
+  state = changeState(state, action)
+  render()
 }
 
 function render() {
-  const app = document.querySelector("#app");
-  app.textContent = state.count;
+  let app = document.getElementById("#app")
+  app.textContent = state.count
 }
+
+dispatch({ type: "@@INIT" })
